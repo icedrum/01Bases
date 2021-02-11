15a63192d18f308963ee15d55c8b728e025b3a62
David <david@myariadna.com>
2021-02-11 16:09:55 +0100

    11/feb/2021

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,14 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface Personaje{
+  nombre: string;
+  poder: number;
+}
+
+
 @Component({
   selector: 'app-main-page',
-  templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  templateUrl: './main-page.component.html'
 })
 export class MainPageComponent  {
 
   constructor() { }
 
+  personajes: Personaje[]=[
+    {nombre:'Goku',poder:10000},{nombre:'Vegeta',poder:7000},
 
+
+  ];
+
+
+  nuevo: Personaje={
+      nombre: '',
+      poder:0,
+  }
+
+
+  cambiarNombre(eve: any){
+    console.log(eve);
+    
+  }
+
+  agregar(){
+
+      if(this.nuevo.nombre.trim().length===0){
+        return;
+      } 
+
+   
+
+      this.personajes.push(this.nuevo);
+      
+      //despues de insertarlo, reinicio
+      this.nuevo={
+        nombre:'',
+        poder:0,
+      }
+      
+    
+  }
 
 }
